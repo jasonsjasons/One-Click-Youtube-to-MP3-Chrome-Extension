77e0639cb138e4bb51bac09d5962af86738b4994
BookSock <jasonLseibel@gmail.com>
2016-06-30 16:08:42 -0400

    Update popup.js

--- OneClickYoutubetoMP3ChromeExtension/popup.js
+++ OneClickYoutubetoMP3ChromeExtension/popup.js
@@ -2,14 +2,38 @@
 
 function hello() {
   console.log("success"); 
-  name=document.getElementById("filename").value;
+  if (savedsetting == 'no') {
+	console.log("saved setting is no");
+	
+	
+	} else {
+		name=document.getElementById("filename").value;
+	}
+  
   chrome.storage.sync.set({'name': name}, function() {
     // saves user inputted file name
 	console.log("name saving was successful"+ name+ " testing123");
   });
   chrome.runtime.sendMessage({greeting: "start"});
 }
+document.addEventListener('DOMContentLoaded', loadoptions);
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('btn').addEventListener('click', hello);//calls hello function after button is clicked
 });
 
+function loadoptions() {
+
+  chrome.storage.sync.get(
+    "savedsetting", function(items) {
+		savedsetting = items.savedsetting;
+		console.log(savedsetting);
+		if (savedsetting == 'no') {
+	console.log("saved setting is no");
+	document.getElementById("wholepopup").innerHTML = "downloading automatically";
+	hello();
+} else if (savedsetting == 'yes') {
+	console.log("saved setting is yes")
+} 
+	} 
+  );
+}
